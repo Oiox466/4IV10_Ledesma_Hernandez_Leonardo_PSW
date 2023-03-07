d20b4e528fa5941be28b8d3f661128eb59d7fda3
Oiox466 <ledesma.hernandez.leonardo6@gmail.com>
2023-03-06 23:04:42 -0600

    algoritmia

--- 06ALGORITMIA/js/logica.js
+++ 06ALGORITMIA/js/logica.js
@@ -1,53 +1,50 @@
 //Problema 1
 function problema1(){
-    
 }
 
 //Problema 2
 function problema2(){
-
 }
 
 //Problema 3
 function problema3(){
-    var alfabetos = ['A-Z','Ñ'];
+  var alfabetos = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
 
-    //Vamos a obtener la entrada de los datos
-    var p3_input = document.querySelector('#p3-input').value;
-    //Lo que necesitamos es separar todo por comas
-    var p3_pabalbras = p3_input.split(',');
+  //Vamos a obtener la entrada de los datos
+  var p3_input = document.querySelector('#p3-input').value;
+  //Lo que necesitamos es separar todo por comas
+  var p3_palabras = p3_input.split(',');
 
-    //Necesitamos una funcion que se encargue de recuperar cada palabra
-    // Tengo que eliminar los espacio
+  //Necesitamos una funcion que se encargue de recuperar cada palabra
+  // Tengo que eliminar los espacio
 
-    p3_pabalbras = p3_pabalbras.map(function (palabras){
-        return palabras.replace(/\s/g,'').toUpperCase();
-    });
+  p3_palabras = p3_palabras.map(function (palabra){
+      return palabra.replace(/\s/g,'').toUpperCase();
+  });
 
-    // Hay que calcular los caracteres únicos
-    var p3_resp = '';
+  // Hay que calcular los caracteres únicos
+  var p3_resp = '';
 
-    // Una funcion que se encargue de calcular que contienen el arreglo y seprar cada caracter y contarlo
-    p3_pabalbras.forEach(function(palabra, i){
-        var letras_unicas = [];
-        var palabra_array = palabra.split('');
+  // Una funcion que se encargue de calcular que contienen el arreglo y seprar cada caracter y contarlo
+  p3_palabras.forEach(function(palabra, i){
+      var letras_unicas = [];
+      var palabra_array = palabra.split('');
 
-        // Debo iterar el alfabeto
-        alfabetos.forEach(function(letra,j){
-            // Itero cada palabra
-            palabra_array.forEach(function(letras_palabras,k){
-                // Comprobar que la letra esté dentro del alfabeto
-                if(letras_palabras == lectura){
-                    // si la letra no la hemos contado la agregamos a un array para contar las letras únicas
-                    if(letras_unicas.indexOf(letra)<0){
-                        letras_unicas.push(letra);
-                    }
-                }
-            });
-        });
-        // Vamos a contar la salida
-        p3_resp += 'Palabra: ' + palabra + ' = ' + letras_unicas.length + '\n';
-    });
-    // imprimir salida
-    document.querySelector('#p3-output').textContent = p3_resp; 
+      // Debo iterar la palabra_array para contar las letras únicas
+      palabra_array.forEach(function(letra, j){
+          // Comprobar que la letra esté dentro del alfabeto
+          if (alfabetos.includes(letra)) {
+              // si la letra no la hemos contado la agregamos a un array para contar las letras únicas
+              if (!letras_unicas.includes(letra)) {
+                  letras_unicas.push(letra);
+              }
+          }
+      });
+
+      // Vamos a contar la salida
+      p3_resp += 'Palabra: ' + palabra + ' = ' + letras_unicas.length + '\n';
+  });
+
+  // imprimir salida
+  document.querySelector('#p3-output').textContent = p3_resp; 
 }
